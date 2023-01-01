413d94c60d839adf3643675f5a1c476c8f97392d
DarwinQuito <97117490+DarwinQuito@users.noreply.github.com>
2023-01-01 17:40:44 -0500

    first commit

---
+++ src/sumIntegersInString.spec.js
@@ -0,0 +1,16 @@
+import { game, GAME_WORDS_SUM } from "./sumIntegersInString";
+
+describe('Sum Integers in String code kata', () => {
+  test('should works', () => {
+    const input = GAME_WORDS_SUM.example1;
+    const value = 4;
+    const resp = game(input);
+    expect(resp).toEqual(value);
+  });
+  test('should works', () => {
+    const input = GAME_WORDS_SUM.example2;
+    const value = 3635;
+    const resp = game(input);
+    expect(resp).toEqual(value);
+  });
+});
